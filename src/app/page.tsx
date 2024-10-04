3bce7f9fed5d90689b480e5475985c071fcb2601
Amna Owais Ansari <aoansari9@gmail.com>
2024-10-04 12:18:16 +0500

    Profile Day 2 Class at GIAIC

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+
+
+export default function Home() {
+  return (
+    <div className="main h-[100px] w-[100px] font-serif ">
+      <div className="menus flex fixed w-full items-center justify-end p-5 rounded-md"><h3>0</h3>
+      <h3>menu</h3>
+      </div>
+      
+    <div className="intro h-full w-full p-[30px]"><h1>Anabiya <br />Little Chef</h1>
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
+    rel="stylesheet"
+/>
+    <div className="place"><h3>(Karachi,Pakistan)</h3>
+    <h3>Scroll Down<i className="ri-arrow-right-down-line"></i></h3>
+    </div>
+    </div>
+    <div className="pic"> <img  src="https://thumbs.dreamstime.com/b/little-chef-her-veggies-beautiful-happily-sitting-barefoot-s-outfit-basket-fulled-fresh-white-background-34434599.jpg" alt="" /><br />
+    
+      <h1>Enjoys creating his own<b>“recipes,”</b> using everything from toy fruits to real veggies, and serving his family imaginative meals with a big, proud smile. </h1>
+    </div>
+    <div className="About1">
+     
+      <i className="ri-instagram-line"><i className="ri-whatsapp-line"></i><i className="ri-linkedin-line"></i></i>
+     </div>
+    </div>
+  )
+
+}
